b8617dc09e6dbac4356c470310764ee18e70ad56
utkarshgautam7j-gif <utkarshgautam7j@gmail.com>
2025-08-12 10:50:01 +0530

    Add files via upload

---
+++ rangeofprimitivetype.java
@@ -0,0 +1,13 @@
+public class rangeofprimitivetype {
+    public static void main(String[] args) {
+        System.out.println("range of byte is " + Byte.MIN_VALUE +" to "+ Byte.MAX_VALUE);
+        System.out.println("range of short is " + Short.MIN_VALUE +" to "+ Short.MAX_VALUE);
+        System.out.println("range of int is " + Integer.MIN_VALUE +" to "+ Integer.MAX_VALUE);
+        System.out.println("range of long is " + Long.MIN_VALUE +" to "+ Long.MAX_VALUE);
+        System.out.println("range of float is " + Float.MIN_VALUE +" to "+ Float.MAX_VALUE);
+        System.out.println("range of double is " +Double.MIN_VALUE +" to "+ Double.MAX_VALUE);
+        System.out.println("range of char is " + (int)Character.MIN_VALUE +" to "+(int) Character.MAX_VALUE);
+        System.out.println("range of char is " + Character.MIN_VALUE +" to "+ Character.MAX_VALUE);
+    }
+    
+}
